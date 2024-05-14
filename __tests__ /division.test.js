2e508c1611e6c2c3de9b8c4a49bf9551c94cb3c9
Ilian BENAISSA <72051246+iliancode@users.noreply.github.com>
2024-05-14 12:40:02 +0200

    test sonar

--- __tests__ /division.test.js
+++ __tests__ /division.test.js
@@ -1,6 +1,6 @@
 function divide(numerator, denominator) {
  if (denominator === 0) {
- throw new Error('Cannot divide by zero');
+ throw new Error('Cannot divide by zero !');
  }
  return numerator / denominator;
 }
